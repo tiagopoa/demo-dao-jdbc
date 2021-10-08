bc5ae42655d7377c4a5abe775ca83ccf91c60887
Tiago Correa <tiagopoa@gmail.com>
2021-10-08 11:15:49 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department obj = new Department(1, "books");
 		
 		Seller seller = new Seller(1, "Alex", "alex@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("Department: " + obj);
 		System.out.println("Seller: " + seller);
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.sql.Connection;
+
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+	
+	
+}
